db3ad0d0ce57ed7e0b79dea323425d5a81bd8fd0
fernando6166 <fernando6166@gmail.com>
2017-05-19 21:05:16 -0300

    Mostrar lista de empleados y crear empleado(todavia no funciona)

--- app/app.js
+++ app/app.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 var app = angular.module('MobileAngularUiExamples', [
@@ -16,4 +17,7 @@ app.config(function ($routeProvider) {
             controller: "MainController"
         });
     $routeProvider.when('/about', {templateUrl: "templates/about.html"});
+    $routeProvider.when('/addEmployee', {templateUrl: "templates/addEmployee.html"});
 });
+    
+
--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,6 +1,10 @@
-
-
-
-app.controller('MainController', function ($scope) {
+'use strict';
+app.controller('MainController', function ($scope,$http) {
+ 	var temp = null;
+   $http.get("https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole")
+    .then(function(response) {
+       $scope.employees = response.data;
+    });
+  	
 
 });
